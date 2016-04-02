e2487d59a60cf5eb6f807c1e6c4fdc6ca36f0f1c
hedelin <1561013230@qq.com>
2016-04-02 10:03:27 +0800

    first commit pro

---
+++ GittDemo/GittDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GittDemo
+//
+//  Created by qianfeng on 16/4/2.
+//  Copyright © 2016年 何得林. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+

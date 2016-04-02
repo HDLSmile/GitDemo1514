ae1116736de22fe3b36571e74e95a7251d0417f9
hedelin <1561013230@qq.com>
2016-04-02 10:29:55 +0800

    A commit changed

---
+++ GittDemo/GittDemo/ViewControllerA.m
@@ -0,0 +1,16 @@
+//
+//  ViewControllerA.m
+//  GittDemo
+//
+//  Created by qianfeng on 16/4/2.
+//  Copyright © 2016年 何得林. All rights reserved.
+//
+
+#import "ViewControllerA.h"
+
+@implementation ViewControllerA
+- (void)viewDidLoad{
+    [super viewDidLoad];
+    self.view.backgroundColor =[UIColor redColor];//A
+}
+@end

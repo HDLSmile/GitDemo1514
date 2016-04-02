ff7c63564075986d72e84aeee8250aca3dfc8507
hedelin <1561013230@qq.com>
2016-04-02 10:39:03 +0800

    B commit changged

---
+++ GittDemo/GittDemo/viewControllerB.m
@@ -0,0 +1,13 @@
+//
+//  viewControllerB.m
+//  GittDemo
+//
+//  Created by qianfeng on 16/4/2.
+//  Copyright © 2016年 何得林. All rights reserved.
+//
+
+#import "viewControllerB.h"
+
+@implementation viewControllerB
+
+@end
